05a1ffab9c217e2baafa4f80f679a4d6097cfe7a
AnierinB <anierin@evolution-x.org>
2024-09-04 18:07:44 -0700

    extras: Add various Lock/Unlock sounds [1/2]
    Signed-off-by: AnierinB <anierin@evolution-x.org>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- extra.mk
+++ extra.mk
@@ -4,6 +4,10 @@ PRODUCT_PACKAGES += \
     PixelLauncherNoGestureHintOverlay
 endif
 
+# Lock/Unlock sounds
+PRODUCT_COPY_FILES += \
+     $(call find-copy-subdir-files,*,vendor/extra/prebuilt/product/media/audio/ui,$(TARGET_COPY_OUT_PRODUCT)/media/audio/ui)
+
 # Fonts
 include vendor/extra/themes/fonts/fonts.mk
 
