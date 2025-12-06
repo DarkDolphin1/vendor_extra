b97f755ea012896ab6f1a9686f3cae8712a7733f
Mrick343 <mrinmoy8429de@gmail.com>
2025-12-07 01:25:40 +0530

    extras: Initial import of themes

--- extra.mk
+++ extra.mk
@@ -3,3 +3,12 @@ ifeq ($(TARGET_INCLUDE_PIXEL_LAUNCHER),true)
 PRODUCT_PACKAGES += \
     PixelLauncherNoGestureHintOverlay
 endif
+
+# Fonts
+include vendor/extra/themes/fonts/fonts.mk
+
+# Icon Packs
+include vendor/extra/themes/iconpacks/iconpacks.mk
+
+# Icon Shapes
+include vendor/extra/themes/iconshapes/iconshapes.mk
